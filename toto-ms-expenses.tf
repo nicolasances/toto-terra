23f1752aca851ad200441824fa5b128cd2acb84d
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-23 20:04:11 +0200

    Added creation of bucket

--- toto-ms-expenses.tf
+++ toto-ms-expenses.tf
@@ -20,3 +20,16 @@ resource "google_project_iam_member" "toto-ms-expenses-role-gcs" {
     role = "roles/storage.admin"
     member = format("serviceAccount:%s", google_service_account.toto-ms-expenses-service-account.email)
 }
+
+# Storage Bucket
+variable "nop_ms_expenses_storage_bucket" {
+    description = "Storage Bucket used for Backup of the Expenses DB"
+    type = string
+}
+
+resource "google_storage_bucket" "backup-bucket" {
+    name = var.nop_ms_expenses_storage_bucket
+    location = "EU"
+    force_destroy = false
+    uniform_bucket_level_access = true
+}
