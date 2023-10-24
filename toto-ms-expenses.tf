6b0987c9952fa7de8e6126cc5eb96fbd3ea810b5
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-24 19:27:41 +0200

    Added bucket for backup of expenses

--- toto-ms-expenses.tf
+++ toto-ms-expenses.tf
@@ -4,6 +4,7 @@ resource "google_service_account" "toto-ms-expenses-service-account" {
   display_name = "Expenses Service Account"
 }
 
+# ---------------------------------------------------------------
 # Provide IAM roles to Service Account
 resource "google_project_iam_member" "toto-ms-expenses-role-secretmanagedaccessor" {
     project = var.gcp_pid
@@ -21,15 +22,20 @@ resource "google_project_iam_member" "toto-ms-expenses-role-gcs" {
     member = format("serviceAccount:%s", google_service_account.toto-ms-expenses-service-account.email)
 }
 
-# Storage Bucket
-variable "nop_ms_expenses_storage_bucket" {
-    description = "Storage Bucket used for Backup of the Expenses DB"
-    type = string
-}
-
+# ---------------------------------------------------------------
+# Storage Bucket to store the backups
 resource "google_storage_bucket" "backup-bucket" {
-    name = var.nop_ms_expenses_storage_bucket
+    name = format("%s-expenses-backup-bucket", var.gcp_pid)
     location = "EU"
     force_destroy = false
     uniform_bucket_level_access = true
+}
+
+# ---------------------------------------------------------------
+# Github environment secrets 
+resource "github_actions_environment_secret" "toto_backup_bucket_envsecret" {
+    repository = "toto-ms-expenses"
+    environment = var.gcp_pid
+    secret_name = "backup-bucket"
+    plaintext_value   = github_actions_environment_secret.toto_backup_bucket_envsecret.name
 }
