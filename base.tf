809f2112f28b69b94567df3935d5b89982985958
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-25 20:09:09 +0200

    Switched github provider version

--- base.tf
+++ base.tf
@@ -6,7 +6,7 @@ terraform {
         }
         github = {
             source  = "integrations/github"
-            version = "4.20.0"
+            version = "5.40.0"
         }
     }
     # backend "gcs" {
