8f3a6721e4cd9ba9a627effca8c9cadf3b43edde
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-29 07:15:47 +0100

    Updated rrdata

--- toto-ms-expenses.tf
+++ toto-ms-expenses.tf
@@ -110,7 +110,7 @@ resource "google_secret_manager_secret_version" "toto-ms-expenses-mongo-pswd-ver
 # ---------------------------------------------------------------
 resource "google_dns_record_set" "api_expenses_dns" {
   name = format("expenses.%s.api.toto.nimatz.com", var.toto_environment)
-  rrdatas = "ghs.googlehosted.com."
+  rrdatas = ["ghs.googlehosted.com."]
   type = "CNAME"
   ttl  = 3600
   managed_zone = "nimatz.com"
