4e75d13b3b73930eaba2dfb42b3e638b97973ccd
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-29 07:16:43 +0100

    Fixed DNS domain name

--- toto-ms-expenses.tf
+++ toto-ms-expenses.tf
@@ -109,7 +109,7 @@ resource "google_secret_manager_secret_version" "toto-ms-expenses-mongo-pswd-ver
 # 5. Cloud DNS
 # ---------------------------------------------------------------
 resource "google_dns_record_set" "api_expenses_dns" {
-  name = format("expenses.%s.api.toto.nimatz.com", var.toto_environment)
+  name = format("expenses.%s.api.toto.nimatz.com.", var.toto_environment)
   rrdatas = ["ghs.googlehosted.com."]
   type = "CNAME"
   ttl  = 3600
