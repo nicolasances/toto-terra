8b4403ea75b6e49dc14aa6a95eac1dfc54249cd9
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-29 07:32:54 +0100

    Fixed DNS & Domain mapping

--- dns.tf
+++ dns.tf
@@ -1,4 +1,4 @@
 resource "google_dns_managed_zone" "dns_zone" {
   name     = format("toto-%s-zone", var.toto_environment)
-  dns_name = format("%s.nimatz.com.", var.toto_environment)
+  dns_name = format("%s.toto.nimatz.com.", var.toto_environment)
 }
