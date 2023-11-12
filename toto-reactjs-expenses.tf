0d8564b7bf159969c8ba911ebd1c3ebcdb302e08
nicolasances <nicolas.matteazzi@gmail.com>
2023-11-12 10:00:07 +0100

    Adding Expenses V2 endpoint to Github secret of reactjs app

---
+++ toto-reactjs-expenses.tf
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------
+# 3. Github environment secrets & variables
+# ---------------------------------------------------------------
+resource "github_actions_environment_secret" "web_expenses_github_secret_expenses_endpoint" {
+    repository = "toto-reactjs-expenses"
+    environment = var.gcp_pid
+    secret_name = "EXPENSESV2_API_ENDPOINT"
+    plaintext_value  = format("https://toto-ms-expenses-%s", var.cloud_run_endpoint_suffix)
+}
