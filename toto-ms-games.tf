a3d1d4283bdd4cc5f0b434d52300efc350bb6724
nicolasances <nicolas.matteazzi@gmail.com>
2023-11-27 07:37:23 +0100

    1.8.0 Games: toto-ms-games

---
+++ toto-ms-games.tf
@@ -0,0 +1,102 @@
+# ---------------------------------------------------------------
+# 1. Service Account 
+# ---------------------------------------------------------------
+# 1.1. Service Account 
+# ---------------------------------------------------------------
+resource "google_service_account" "toto-ms-games-service-account" {
+  account_id = "toto-ms-games"
+  display_name = "Expenses Service Account"
+}
+
+# ---------------------------------------------------------------
+# 1.2. Service Account Roles
+# ---------------------------------------------------------------
+resource "google_project_iam_member" "toto-ms-games-role-secretmanagedaccessor" {
+    project = var.gcp_pid
+    role = "roles/secretmanager.secretAccessor"
+    member = format("serviceAccount:%s", google_service_account.toto-ms-games-service-account.email)
+}
+resource "google_project_iam_member" "toto-ms-games-role-gcs" {
+    project = var.gcp_pid
+    role = "roles/storage.admin"
+    member = format("serviceAccount:%s", google_service_account.toto-ms-games-service-account.email)
+}
+resource "google_project_iam_member" "toto-ms-games-role-pubsub" {
+    project = var.gcp_pid
+    role = "roles/pubsub.publisher"
+    member = format("serviceAccount:%s", google_service_account.toto-ms-games-service-account.email)
+}
+
+# ---------------------------------------------------------------
+# 2. Storage Bucket 
+# ---------------------------------------------------------------
+
+# ---------------------------------------------------------------
+# 3. Github environment secrets & variables
+# ---------------------------------------------------------------
+resource "github_repository_environment" "toto-ms-games-github-environment" {
+    repository = "toto-ms-games"
+    environment = var.gcp_pid
+}
+resource "github_actions_environment_secret" "totomsgames-secret-cicdsakey" {
+    repository = "toto-ms-games"
+    environment = var.gcp_pid
+    secret_name = "CICD_SERVICE_ACCOUNT"
+    plaintext_value = jsonencode(jsondecode(base64decode(google_service_account_key.toto-cicd-sa-key.private_key)))
+}
+resource "github_actions_environment_variable" "totomsgames-var-pid" {
+    repository = "toto-ms-games"
+    environment = var.gcp_pid
+    variable_name = "GCP_PID"
+    value = var.gcp_pid
+}
+resource "github_actions_environment_secret" "totomsgames-secret-service-account" {
+    repository = "toto-ms-games"
+    environment = var.gcp_pid
+    secret_name = "SERVICE_ACCOUNT"
+    plaintext_value = google_service_account.toto-ms-games-service-account.email
+}
+
+# ---------------------------------------------------------------
+# 4. Google Secret Manager (Secrets)
+# ---------------------------------------------------------------
+variable "toto_ms_games_mongo_user" {
+    description = "Mongo User for games"
+    type = string
+    sensitive = true
+}
+variable "toto_ms_games_mongo_pswd" {
+    description = "Mongo Password for games"
+    type = string
+    sensitive = true
+}
+resource "google_secret_manager_secret" "toto-ms-games-mongo-user" {
+    secret_id = "toto-ms-games-mongo-user"
+    replication {
+        auto { }
+    }
+}
+resource "google_secret_manager_secret_version" "toto-ms-games-mongo-user-version" {
+    secret = google_secret_manager_secret.toto-ms-games-mongo-user.id
+    secret_data = var.toto_ms_games_mongo_user
+}
+resource "google_secret_manager_secret" "toto-ms-games-mongo-pswd" {
+    secret_id = "toto-ms-games-mongo-pswd"
+    replication {
+        auto { }
+    }
+}
+resource "google_secret_manager_secret_version" "toto-ms-games-mongo-pswd-version" {
+    secret = google_secret_manager_secret.toto-ms-games-mongo-pswd.id
+    secret_data = var.toto_ms_games_mongo_pswd
+}
+
+# ---------------------------------------------------------------
+# 5. Cloud DNS & Domain Mapping
+# ---------------------------------------------------------------
+# 5.1. DNS 
+# 5.2. Domain Mapping
+
+# ---------------------------------------------------------------
+# 6. PubSub Subscriptions to events
+# ---------------------------------------------------------------
