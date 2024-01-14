aaf5030d5f3e8cfd8b5f532c5d2f74b53d206077
nicolasances <nicolas.matteazzi@gmail.com>
2024-01-14 14:26:37 +0100

    Incast endpoint

--- toto-reactjs-expenses.tf
+++ toto-reactjs-expenses.tf
@@ -13,3 +13,9 @@ resource "github_actions_environment_secret" "web_expenses_github_secret_games_e
     secret_name = "GAMES_API_ENDPOINT"
     plaintext_value  = format("https://toto-ms-games-%s", var.cloud_run_endpoint_suffix)
 }
+resource "github_actions_environment_secret" "web_expenses_github_secret_games_endpoint" {
+    repository = "toto-reactjs-expenses"
+    environment = var.toto_environment
+    secret_name = "INCAST_API_ENDPOINT"
+    plaintext_value  = format("https://toto-ml-incast-%s", var.cloud_run_endpoint_suffix)
+}
