1dfbced247b2d647d9727578a3601120d9617432
nicolasances <nicolas.matteazzi@gmail.com>
2024-12-28 08:13:39 +0100

    Added a bucket for Tome

--- gcs.tf
+++ gcs.tf
@@ -19,3 +19,9 @@ resource "google_storage_bucket" "supermarket-backup-bucket" {
     force_destroy = false
     uniform_bucket_level_access = true
 }
+resource "google_storage_bucket" "tome-bucket" {
+    name = format("%s-tome-bucket", var.gcp_pid)
+    location = "EU"
+    force_destroy = false
+    uniform_bucket_level_access = true
+}
