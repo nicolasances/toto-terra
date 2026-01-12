e93311091303d7b9b1c5eddda5b19432be852c7b
nicolasances <nicolas.matteazzi@gmail.com>
2026-01-12 08:47:56 +0100

    Most recent Google Provider

--- base.tf
+++ base.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         google = {
             source = "hashicorp/google"
-            version = "5.3.0"
+            version = "7.15.0"
         }
         github = {
             source  = "integrations/github"
