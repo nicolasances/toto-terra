0c8a6d490dca7a662ecf07f13ae0781284552e95
nicolasances <nicolas.matteazzi@gmail.com>
2026-03-21 09:13:03 +0100

    Added Gale Broker basepath

--- toto-react-supermarket.tf
+++ toto-react-supermarket.tf
@@ -45,7 +45,7 @@ resource "github_actions_environment_secret" "web_suppie_github_secret_gale_brok
     repository = "toto-suppie"
     environment = var.toto_environment
     secret_name = "GALE_BROKER_API_ENDPOINT"
-    plaintext_value  = format("https://gale-broker-%s", var.cloud_run_endpoint_suffix)
+    plaintext_value  = format("https://gale-broker-%s/galebroker", var.cloud_run_endpoint_suffix)
 }
 resource "github_actions_environment_secret" "web_suppie_github_secret_whispering_endpoint" {
     repository = "toto-suppie"
